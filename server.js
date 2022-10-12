b5d0f960b2116f36c2de3775461954b7d552067c
hasinsadique <hasinsadique7@gmail.com>
2022-10-12 23:39:46 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log("Server running and listening on port ", PORT);
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello developer Welcome to Game of Nature server.");
+});

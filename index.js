59c520693f4bbd85a6b27934774601eb69e68350
hasinsadique <hasinsadique7@gmail.com>
2022-10-14 20:35:56 +0600

    reset-2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 const cors = require("cors");
 
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-client.connect();
+// client.connect();
+// console.log();
 
 app.listen(PORT, (error) => {
     if (!error) {

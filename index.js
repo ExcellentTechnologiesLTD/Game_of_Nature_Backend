83eed63394124f7316392554a704e9fe439caa34
hasinsadique <hasinsadique7@gmail.com>
2022-11-11 03:26:11 +0600

    add and delete item api done

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 const cors = require("cors");
 
 const client = require("./connection");
+const { query } = require("./connection");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
 
 app.use(cors());
 app.use(express.json({ extended: false }));
@@ -33,4 +34,50 @@ app.get("/", (req, res) => {
     });
     client.end;
     // res.send("Hello developer Welcome to Game of Nature server. Bhalo Achen??");
+});
+
+app.post("/add-item", (req, res) => {
+    const { itemName, category, quantity, price, description, imgUrl } = req.body;
+    console.log(imgUrl);
+    queryInsertItem = `INSERT INTO public.products(
+        product_name, quantity_in_stock, price, category, description, photo_url)
+        VALUES ('${itemName}', '${quantity}', '${price}', '${category}', '${description}','${imgUrl}')
+        RETURNING product_id;
+        ;`;
+
+    client.query(queryInsertItem, (err, result) => {
+        if (!err) {
+            // console.log(result.rows[0].product_id);
+            if (result.rowCount == 1 && result.rows[0].product_id) {
+                res.send({
+                    status: "Success",
+                    msg: "Item Added Successfully",
+                    product_id: result.rows[0].product_id,
+                });
+            } else {
+                res.send({
+                    status: "Error",
+                    msg: "Could not add item. Call Developer.",
+                });
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.delete("/delete-item/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    const queryDeleteItem = `DELETE FROM public.products
+	WHERE "product_id"=${id};`;
+
+    client.query(queryDeleteItem, (err, result) => {
+        if (!err) {
+            res.send({ msg: "Deleted successfully", deleted: true });
+        } else {
+            console.log(err);
+        }
+    });
 });

04883fdb1db1f503ae9e9748ad9b77addb2c0e3f
hasinsadique <hasinsadique7@gmail.com>
2022-12-15 00:06:01 +0600

    Added new apis.

--- index.js
+++ index.js
@@ -20,6 +20,72 @@ app.listen(PORT, (error) => {
     }
 });
 
+app.post("/signup", (req, res) => {
+    const { firstName, lastName, address, email, phoneNumber, password } =
+    req.body;
+
+    console.log(req.body);
+
+    queryInsertNewUser = `INSERT INTO public."Users"(
+        "First_Name", "Email", "Mobile", "Address", "User_Type", "Last_Name", "password")
+        VALUES ('${firstName}', '${email}', '${phoneNumber}', '${address}', 'Customer', '${lastName}', '${password}')
+        RETURNING user_id;`;
+
+    client.query(queryInsertNewUser, (err, result) => {
+        if (!err) {
+            if (result.rowCount == 1) {
+                console.log("User registered.");
+                res.send({
+                    status: "Success",
+                    msg: "User Registered Successfully",
+                    user_id: result.rows[0].user_id,
+                });
+            } else {
+                console.log("Not Registered.");
+                res.send({
+                    status: "Error",
+                    msg: "Could not register user. Call Developer.",
+                });
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/signin", (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+
+    let queryCheckUser = `SELECT * FROM public."Users" Where "Email" = '${email}'`;
+
+    client.query(queryCheckUser, (err, result) => {
+        if (!err) {
+            if (result.rows.length > 0 && result.rows[0].Email == email) {
+                if (result.rows[0].password == password) {
+                    res.send({
+                        status: "Success",
+                        msg: "Correct User Info.",
+                        currentUserInfo: result.rows[0],
+                    });
+                } else {
+                    res.send({
+                        status: "Error",
+                        msg: "Incorrect Password. Try Again",
+                    });
+                }
+            } else {
+                res.send({
+                    status: "Error",
+                    msg: "Incorrect email or password.",
+                });
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 app.get("/get-details/:id", (req, res) => {
     const id = req.params.id;
     console.log(id);
@@ -35,8 +101,23 @@ app.get("/get-details/:id", (req, res) => {
     client.end;
 });
 
+app.get("/get-detailsby/:category", (req, res) => {
+    const category = req.params.category;
+    console.log(category);
+    let query = `SELECT * FROM public.products where category = '${category}';`;
+
+    client.query(query, (err, result) => {
+        if (!err) {
+            res.send(result.rows);
+        } else {
+            console.log(err);
+        }
+    });
+    client.end;
+});
+
 app.get("/", (req, res) => {
-    let query = `SELECT * FROM public.products`;
+    let query = `SELECT * FROM public.products order by product_id asc`;
 
     // res.send("Hello developer welcome to teebay Backend.");
     client.query(query, (err, result) => {
